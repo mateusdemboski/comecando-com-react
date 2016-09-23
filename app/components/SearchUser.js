6902e24a77555e4764d0e129e80c4d307b87828a
Mateus Demboski <mateus.demboski@digitaldesk.com.br>
2016-09-23 15:10:38 -0300

    Comunicação entre Components

--- app/components/SearchUser.js
+++ app/components/SearchUser.js
@@ -4,11 +4,13 @@ var GitHubUser = require('../services/GitHubUser');
 var SearchUser = React.createClass({
     handleSubmit: function(e) {
         e.preventDefault();
-        GitHubUser.getByUsername(this.refs.username.value)
-        .then(function(response) { console.log(response) });
+        GitHubUser.getByUsername(this.refs.username.value).then(function(response) { 
+            this.props.updateUser(response.data) 
+        }.bind(this));
 
-        GitHubUser.getReposByUsername(this.refs.username.value)
-        .then(function(response) { console.log(response) });
+        GitHubUser.getReposByUsername(this.refs.username.value).then(function(response) { 
+            this.props.updateRepos(response.data)
+        }.bind(this));
     },
 
     render: function() {
