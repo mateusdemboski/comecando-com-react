cbe3073d3bbc8e85c7ddab363f01775a9e4713cf
Mateus Demboski <mateus.demboski@digitaldesk.com.br>
2016-09-26 09:55:58 -0300

    Component LifeCycle

--- app/components/UserRepos.js
+++ app/components/UserRepos.js
@@ -1,6 +1,14 @@
 var React = require('react');
 
 var UserRepos = React.createClass({
+    getInitialState: function() {
+      return {
+        reposCount: 0,
+      }
+    },
+    componentWillReceiveProps: function(props) {
+      this.setState({reposCount: props.repos.length});
+    },
     render: function() {
         var repos = this.props.repos.map(function(repo, key) {
           return (
@@ -11,7 +19,7 @@ var UserRepos = React.createClass({
                 </h3>
                 <p>{repo.description}</p>
                 <p>
-                  <a href={repo.htm_url} className="btn btn-primary" role="button">Repository</a>
+                  <a href={repo.htlm_url} className="btn btn-primary" role="button">Repository</a>
                   <a href={repo.html_url + '/issues'} className="btn btn-default" role="button">Issues ({repo.open_issues})</a>
                 </p>
               </div>
@@ -21,6 +29,7 @@ var UserRepos = React.createClass({
 
         return (
           <div>
+            <h2>{this.state.reposCount} Repositories</h2>
             {repos}
           </div>
         );
