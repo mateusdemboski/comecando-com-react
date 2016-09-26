da0533df060bc89bec2e468ac27dd860233d91a4
Mateus Demboski <mateus.demboski@digitaldesk.com.br>
2016-09-26 08:57:01 -0300

    Iterando e Renderizando com Loops

---
+++ app/components/UserRepos.js
@@ -0,0 +1,30 @@
+var React = require('react');
+
+var UserRepos = React.createClass({
+    render: function() {
+        var repos = this.props.repos.map(function(repo, key) {
+          return (
+            <div key={key} className="thumbnail">
+              <div className="caption">
+                <h3>{repo.name}
+                  <span className="badge">{repo.stargazers_count} STARTS</span>
+                </h3>
+                <p>{repo.description}</p>
+                <p>
+                  <a href={repo.htm_url} className="btn btn-primary" role="button">Repository</a>
+                  <a href={repo.html_url + '/issues'} className="btn btn-default" role="button">Issues ({repo.open_issues})</a>
+                </p>
+              </div>
+            </div>
+          );
+        });
+
+        return (
+          <div>
+            {repos}
+          </div>
+        );
+    }
+});
+
+module.exports = UserRepos;
